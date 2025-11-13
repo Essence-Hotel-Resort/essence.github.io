b6894852f506c7deb8cc3922b97a07498df75e7b
CaydinDev8431 <156533697+CaydinDev8431@users.noreply.github.com>
2025-11-13 01:16:16 -0500

    🚩 essence.v1.03

---
+++ config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqT-2OppehOjHF-JETMCjreAZkZ-iA_Z8",
+  authDomain: "fivem-server-sophoswrld.firebaseapp.com",
+  projectId: "fivem-server-sophoswrld",
+  storageBucket: "fivem-server-sophoswrld.firebasestorage.app",
+  messagingSenderId: "590745467368",
+  appId: "1:590745467368:web:42f1a4a41f9ac7d33a8c5d",
+  measurementId: "G-0XMVYH41J2"
+};
+
+export { firebaseConfig }
